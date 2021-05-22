f653580e3a2d0f77bf75bb6d56ed858be640e87c
IanMFraser <hunter_gracchus@hotmail.com>
2021-05-22 09:17:57 -0400

    old news section

--- src/components/News.js
+++ src/components/News.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import styles from'./News.module.css'
 import data from '../data.json'
 import classNames from 'classnames'
 import Badge from 'react-bootstrap/Badge'
 import Card from 'react-bootstrap/Card'
+import styles from './News.module.css'
+
 const News = () => {
 
     const currentNews = data.news.currentNews
@@ -14,26 +15,14 @@ const News = () => {
             <Card.Header as="h1" style={{border:"none", backgroundColor:'#f8f8f8'}}>News</Card.Header>
             <Card.Body>
                 <Card.Title className="mx-2">{currentNews.title}</Card.Title>
-                <Card.Subtitle className="ml-2 text-muted">{currentNews.date} <Badge pill variant="info">{currentNews.category}</Badge></Card.Subtitle>
+                <Card.Subtitle className="ml-2 text-muted">
+                    {currentNews.date} 
+                    <Badge pill variant="info">{currentNews.category}</Badge>
+                </Card.Subtitle>
                 <Card.Img src={currentNews.images} style={{width: '320px', height: '320px'}} className="mt-4"></Card.Img>
                 <Card.Text className="mt-4">{currentNews.content} {currentNews.link && <a className={linkClassNames} href={currentNews.link} target="_blank" rel="noreferrer">{ '>>'} </a>}</Card.Text>
             </Card.Body>
         </Card>
-        // <Container >
-        //     <h1 >News</h1>
-        //     <div >
-        //         <h2>{currentNews.title}</h2>
-        //     </div>
-        //     <div >{currentNews.date} 
-        //         <span > <Badge pill variant="info">{currentNews.category}</Badge></span>
-        //     </div>
-        //     <div >
-        //         <Image src={currentNews.images} alt={currentNews.alt} thumbnail></Image>
-        //     </div>
-        //     <div>
-        //         {`${currentNews.content}`} {currentNews.link && <a className={linkClassNames} href={currentNews.link} target="_blank" rel="noreferrer">{ '>>'} </a>}
-        //     </div>
-        // </Container>
     )
 }
 
--- src/components/OldNews.js
+++ src/components/OldNews.js
@@ -2,20 +2,19 @@ import React from 'react'
 import data from '../data.json'
 import styles from './OldNews.module.css'
 import classNames from 'classnames'
+import ListGroup from 'react-bootstrap/ListGroup'
 
 const OldNews = () => {
     const oldNews = data.news.oldNews
     const linkClassNames = classNames(styles.oldNewsLink, styles.hvrBounceIn)
 
     return(
-        <div className={styles.oldNews}>
-            <h3>OLD NEWS</h3>
-            <ul>
+        <ListGroup variant="flush" as="ul">
+            <h2 className="m-3">OLD NEWS</h2>
                 {
-                    oldNews.map((n) => <li key={n.id}><strong>{`${n.date} ${n.category}`}</strong>{ ` ${n.content}`} { n.link && <a className={linkClassNames} href={n.link} target="_blank" rel="noreferrer">{ '>>'} </a>} </li>)
+                    oldNews.map((n) => <ListGroup.Item key={n.id} style={{backgroundColor: '#f8f8f8'}} as="li"><strong>{`${n.date} ${n.category}`}</strong>{ ` ${n.content}`} { n.link && <a className={linkClassNames} href={n.link} target="_blank" rel="noreferrer">{ '>>'} </a>} </ListGroup.Item>)
                 }
-            </ul>
-        </div>
+        </ListGroup>
     )
 }
 
