c0a38603ea8d39ed55a3d552904e48e813c31c6a
IanMFraser <hunter_gracchus@hotmail.com>
2021-05-22 12:04:32 -0400

    contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from './Contact.module.css'
+import Figure from 'react-bootstrap/Figure'
 
 const Contact = () => {
     return(
         <div className={styles.contact}>
             <div className={styles.contactAbout}>
-                <figure className={styles.contactImage}>
-                    <img src="https://storage.googleapis.com/album-images/IanFraser_TransPecos-20150805-04317.jpeg" alt="imf performing at trans pecos"></img>
-                    <figcaption><span className={styles.photoCredit}>Photo by Billy Gomberg</span></figcaption>
-                </figure>
+                <Figure>
+                    <Figure.Image src="https://storage.googleapis.com/album-images/IanFraser_TransPecos-20150805-04317.jpeg" alt="imf performing at trans pecos" className={styles.contactImage}></Figure.Image>
+                    <Figure.Caption><span className={styles.photoCredit}>Photo by Billy Gomberg</span></Figure.Caption>
+                </Figure>
                 <p className={styles.contactBio}>
                     Ian M Fraser (b. 1980) is a composer working in the field of computer music. His work primarily focuses on generative structures, stochastic processes, and nonlinear feedback systems.
                     He lives in New York City with his wife and cat.
--- src/components/OldNews.js
+++ src/components/OldNews.js
@@ -3,18 +3,20 @@ import data from '../data.json'
 import styles from './OldNews.module.css'
 import classNames from 'classnames'
 import ListGroup from 'react-bootstrap/ListGroup'
-
+import Badge from 'react-bootstrap/Badge'
 const OldNews = () => {
     const oldNews = data.news.oldNews
     const linkClassNames = classNames(styles.oldNewsLink, styles.hvrBounceIn)
 
     return(
-        <ListGroup variant="flush" as="ul">
+        <>
             <h2 className="m-3">OLD NEWS</h2>
-                {
-                    oldNews.map((n) => <ListGroup.Item key={n.id} style={{backgroundColor: '#f8f8f8'}} as="li" className="mt-4"><strong>{`${n.date} ${n.category}`}</strong>{ ` ${n.content}`} { n.link && <a className={linkClassNames} href={n.link} target="_blank" rel="noreferrer">{ '>>'} </a>} </ListGroup.Item>)
-                }
-        </ListGroup>
+            <ListGroup variant="flush" as="ul">
+                    {
+                        oldNews.map((n) => <ListGroup.Item key={n.id} style={{backgroundColor: '#f8f8f8'}} as="li" className="mt-4"><strong>{`${n.date}`}<Badge variant={n.categoryVariant} className="mx-2">{n.category}</Badge></strong>{ ` ${n.content}`} { n.link && <a className={linkClassNames} href={n.link} target="_blank" rel="noreferrer">{ '>>'} </a>} </ListGroup.Item>)
+                    }
+            </ListGroup>
+        </>
     )
 }
 
