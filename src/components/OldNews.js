699323cf183b67e658d450985601791e3ff8352b
IanMFraser <hunter_gracchus@hotmail.com>
2021-06-21 11:30:45 -0400

    semantics

--- src/components/OldNews.js
+++ src/components/OldNews.js
@@ -2,31 +2,32 @@ import React from 'react'
 import data from '../data.json'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Badge from 'react-bootstrap/Badge'
+import Card from 'react-bootstrap/Card'
 
 const OldNews = () => {
 
     const oldNews = data.news.oldNews
 
     return(
-        <>
-            <h2 className="m-3">OLD NEWS</h2>
-            <ListGroup variant="flush" as="ul">
-                    {
-                        oldNews.map(news => {
-                            return <ListGroup.Item key={news.id} as="li" className="mt-2 border-0">
-                                <strong>
-                                    {`${news.date}`}
-                                    <Badge variant={news.categoryVariant} className="mx-2">
-                                        {news.category}
-                                    </Badge>
-                                </strong>
-                                { ` ${news.content} `} 
-                                { news.link && <a className="newsLink hvrBounceIn" href={news.link} target="_blank" rel="noreferrer">{ ' >>'} </a>} 
-                            </ListGroup.Item>
-                        })
-                    }
-            </ListGroup>
-        </>
+        <Card className="border-0">
+            <Card.Header as="h2" className="border-0">OLD NEWS</Card.Header>
+                <ListGroup variant="flush" as="ul">
+                        {
+                            oldNews.map(news => {
+                                return <ListGroup.Item key={news.id} as="li" className="mt-2 border-0">
+                                    <strong>
+                                        {`${news.date}`}
+                                        <Badge variant={news.categoryVariant} className="mx-2">
+                                            {news.category}
+                                        </Badge>
+                                    </strong>
+                                    { ` ${news.content} `}
+                                    { news.link && <a className="newsLink hvrBounceIn" href={news.link} target="_blank" rel="noreferrer">{ ' >>'} </a>}
+                                </ListGroup.Item>
+                            })
+                        }
+                </ListGroup>
+        </Card>
     )
 }
 
